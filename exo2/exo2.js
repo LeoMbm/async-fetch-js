0ba07f194ee350818eccc3c822f6bbe80ee59f10
Leo Mbm <leonidas.j.mbm@gmail.com>
2022-05-12 15:32:48 +0200

    first features

--- exo2/exo2.js
+++ exo2/exo2.js
@@ -2,6 +2,8 @@
 
 const input = document.querySelector("#search-bar");
 const btn = document.querySelector("button");
+const select = document.querySelector("select");
+const options = document.querySelector("option");
 
 // LISTENERS
 btn.addEventListener("click", sendSearch);
@@ -10,9 +12,11 @@ btn.addEventListener("click", sendSearch);
 function sendSearch(e) {
   e.preventDefault();
   const value = input.value;
-  const fetchInput = (name) => fetch("https://api.agify.io/?name=" + name);
+  const country = select.value;
+  const fetchInput = (name, country) =>
+    fetch("https://api.agify.io/?name=" + name + "&country_id=" + country);
 
-  fetchInput(value)
+  fetchInput(value, country)
     .then((res) => res.json())
     .then((data) => {
       const div = document.createElement("div");
@@ -20,13 +24,16 @@ function sendSearch(e) {
       const resultName = document.createElement("p");
       const resultAge = document.createElement("p");
       const resultCount = document.createElement("p");
+      const resultCountry = document.createElement("p");
       resultName.innerHTML = "Name: " + data.name;
       resultAge.innerHTML = "Age: " + data.age;
       resultCount.innerHTML = "Count: " + data.count;
+      resultCountry.innerHTML = "Country: " + data.country_id;
 
       document.body.appendChild(div);
       div.appendChild(resultName);
       div.appendChild(resultAge);
       div.appendChild(resultCount);
+      div.appendChild(resultCountry);
     });
 }
